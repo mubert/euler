c959e71fa033fde83a1f2619eab34a1ce46aab33
mubert <lowin.mike@gmail.com>
2017-05-13 19:10:48 -0400

    adding q

--- euler15.swift
+++ euler15.swift
@@ -1,6 +1,13 @@
-//: Playground - noun: a place where people can play
+/*
+ https://projecteuler.net/problem=15
+ 
+ Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
 
-import UIKit
+ [diagram]
+ 
+ How many such routes are there through a 20×20 grid?
+*/
+
 
 // Using Ints would overflow for the massive values involved.
 func factorial(_ num: Int) -> Double {
